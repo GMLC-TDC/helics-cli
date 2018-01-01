25f81fcfec7711fd9445c40b4018ac6023b79562
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2018-01-01 12:02:13 -0700

    Rename to helicsrunner and add helics cli

--- setup.py
+++ setup.py
@@ -3,48 +3,53 @@ from setuptools import setup, find_packages
 import os
 from codecs import open
 
-here = os.path.abspath(os.path.dirname(__file__))
+HERE = os.path.abspath(os.path.dirname(__file__))
 
 # Get the long description from the README file
-with open(os.path.join(here, './README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+with open(os.path.join(HERE, './README.md'), encoding='utf-8') as f:
+    LONG_DESCRIPTION = f.read()
 
-with open(os.path.join(here, 'helics_runner', '_version.py'), encoding='utf-8') as f:
-    version = f.read()
+with open(os.path.join(HERE, 'helicsrunner', '_version.py'), encoding='utf-8') as f:
+    VERSION = f.read()
 
-version = version.split()[2].strip('"').strip("'")
-
+VERSION = VERSION.split()[2].strip('"').strip("'")
 
 setup(
-    name='helics_runner',
-
-    version=version,
-
+    name='helicsrunner',
+    version=VERSION,
     description='Python direct-mode interface to OpenDSS',
-    long_description=long_description,
-
+    long_description=LONG_DESCRIPTION,
     url='https://github.com/GMLC-TDC/helics-runner',
     download_url='https://github.com/GMLC-TDC/helics-runner',
 
     # Author details
     author='Dheepak Krishnamurthy',
-    author_email='dheepak.krishnamurthy@nrel.gov',
-
     license='BSD-compatible',
-
     packages=find_packages(),
     install_requires=[
         "future",
         "six",
         "click",
     ],
-
     keywords=['helics', 'cosimulation'],
+    entry_points={
+        "console_scripts": [
+            "helics = helicsrunner.cli:cli",
+        ],
+    },
+    extras_require={
+        "dev": [
+            "pytest",
+            "pytest-cov",
+            "sphinx-rtd-theme",
+            "nbsphinx",
+            "sphinx",
+        ],
+    },
 
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
-
     zip_safe=True,
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
