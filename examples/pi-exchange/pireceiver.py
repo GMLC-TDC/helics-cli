c43041a3afbe5b392938bf830f47789ad34f2ffe
Dheepak Krishnamurthy <me@kdheepak.com>
2020-05-07 12:35:25 -0600

    Update examples

--- examples/pi-exchange/pireceiver.py
+++ examples/pi-exchange/pireceiver.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-import helics as h
 import sys
+import helics as h
 
 helicsversion = h.helicsGetVersion()
 
@@ -9,37 +9,25 @@ federate_name = f"Reciever Federate {sys.argv[1]}"
 print(f"{federate_name}: Helics version = {h.helicsGetVersion()}")
 
 
-# Create Federate Info object that describes the federate properties */
 print(f"{federate_name}: Creating Federate Info")
 fedinfo = h.helicsCreateFederateInfo()
 
-# Set Federate name
 print(f"{federate_name}: Setting Federate Info Name")
 h.helicsFederateInfoSetCoreName(fedinfo, federate_name)
 
-# Set core type from string
 print(f"{federate_name}: Setting Federate Info Core Type")
 h.helicsFederateInfoSetCoreTypeFromString(fedinfo, "zmq")
 
-# Federate init string
 print(f"{federate_name}: Setting Federate Info Init String")
 h.helicsFederateInfoSetCoreInitString(fedinfo, "--federates=1")
 
-# Set the message interval (timedelta) for federate. Note that
-# HELICS minimum message time interval is 1 ns and by default
-# it uses a time delta of 1 second. What is provided to the
-# setTimedelta routine is a multiplier for the default timedelta.
-
-# Set one second message interval
 print(f"{federate_name}: Setting Federate Info Time Delta")
 h.helicsFederateInfoSetTimeProperty(fedinfo, h.helics_property_time_delta, 0.01)
 
-# Create value federate
 print(f"{federate_name}: Creating Value Federate")
 vfed = h.helicsCreateValueFederate(federate_name, fedinfo)
 print(f"{federate_name}: Value federate created")
 
-# Subscribe to PI SENDER's publication
 sub = h.helicsFederateRegisterSubscription(vfed, f"topic{sys.argv[1]}", "")
 print(f"{federate_name}: Subscription registered")
 
@@ -56,9 +44,7 @@ while currenttime <= 100:
     currenttime = h.helicsFederateRequestTime(vfed, 100)
 
     value = h.helicsInputGetString(sub)
-    print(
-        f"{federate_name}: Received value = {value} at time {currenttime} from PI SENDER"
-    )
+    print(f"{federate_name}: Received value = {value} at time {currenttime}")
 
 h.helicsFederateFinalize(vfed)
 
--- examples/pi-exchange/pisender.py
+++ examples/pi-exchange/pisender.py
@@ -1,48 +1,35 @@
 # -*- coding: utf-8 -*-
 import time
-import helics as h
 import sys
+
 from math import pi
+import helics as h
 
 federate_name = f"Sender Federate {sys.argv[1]}"
 
 print(f"{federate_name}: Helics version = {h.helicsGetVersion()}")
 
-# Create Federate Info object that describes the federate properties #
 fedinfo = h.helicsCreateFederateInfo()
 
-# Set Federate name #
 h.helicsFederateInfoSetCoreName(fedinfo, federate_name)
 
-# Set core type from string #
 h.helicsFederateInfoSetCoreTypeFromString(fedinfo, "zmq")
 
-# Federate init string #
 h.helicsFederateInfoSetCoreInitString(fedinfo, "--federates=1")
 
-# Set the message interval (timedelta) for federate. Note th#
-# HELICS minimum message time interval is 1 ns and by default
-# it uses a time delta of 1 second. What is provided to the
-# setTimedelta routine is a multiplier for the default timedelta.
-
-# Set one second message interval #
 h.helicsFederateInfoSetTimeProperty(fedinfo, h.helics_property_time_delta, 0.01)
 
-# Create value federate #
 vfed = h.helicsCreateValueFederate(federate_name, fedinfo)
 print(f"{federate_name}: Value federate created")
 
-# Register the publication #
 pub = h.helicsFederateRegisterGlobalTypePublication(
     vfed, f"topic{sys.argv[1]}", "double", ""
 )
 print(f"{federate_name}: Publication registered")
 
-# Enter execution mode #
 h.helicsFederateEnterExecutingMode(vfed)
 print(f"{federate_name}: Entering execution mode")
 
-# This federate will be publishing deltat*pi for numsteps steps #
 this_time = 0.0
 value = pi
 
